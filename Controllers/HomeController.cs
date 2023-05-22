d5d10e2d94ee51aaa9ad0aff5348829371ec43da
Mohammeedalyazidi <133086580+Mohammeedalyazidi@users.noreply.github.com>
2023-05-22 14:48:03 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using home2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace home2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+       
+        public IActionResult about()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
+        {
+            return View();
+        }
+
+        public IActionResult services()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Single()
+        {
+            return View();
+        }
+
+    }
+    }
